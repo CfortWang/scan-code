c055800242821de12727d3f5caf7d6ee6390f50c
jeon <jeon@vinnsmail.com>
2018-07-02 14:41:32 +0900

    beta build

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // api_url: '"http://dev-was.qr3355dev.com"',
-  api_url: '"http://jeon-bpwas.beanpop.cn"',
+  api_url: '"http://beta-was.beanpop.cn"',
   appdown_url: '"http://android.myapp.com/myapp/detail.htm?apkName=cn.beanpop.userapp"'
 }
