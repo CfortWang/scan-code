7c19b8cf64c58d0c626be8198a72fee05e3f3eb8
CfortWang <cfortwang@gmail.com>
2018-07-16 11:25:14 +0800

    cfort

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // api_url: '"http://dev-was.qr3355dev.com"',
-  api_url: '"http://bpwas.beanpop.cn"',
+  api_url: '"http://bpapi.beanpop.cn"',
   appdown_url: '"http://android.myapp.com/myapp/detail.htm?apkName=cn.beanpop.userapp"'
 }
