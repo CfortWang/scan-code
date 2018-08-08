d79c2f454155b62fab9ed507992f25268424537b
CfortWang <cfortwang@gmail.com>
2018-08-08 16:45:44 +0800

    cfort

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // api_url: '"https://bpwas.beanpop.cn"',
-  api_url: '"http://dev-bpapi.beanpop.cn"',
+  api_url: '"https://bpapi.beanpop.cn"',
+  // api_url: '"http://dev-bpapi.beanpop.cn"',
   appdown_url: '"http://android.myapp.com/myapp/detail.htm?apkName=cn.beanpop.userapp"'
 })
