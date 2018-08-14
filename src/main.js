b50cb1b4a772700bea97f03045debba2bfb772f3
CfortWang <cfortwang@gmail.com>
2018-08-14 17:16:04 +0800

    cfort

--- src/main.js
+++ src/main.js
@@ -15,3 +15,29 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+// router.beforeEach((to, from, next) => {
+//   setTimeout(() => {
+//     let _hmt = _hmt || []
+//     window._hmt = _hmt
+//     (() => {
+//       document.getElementById('baidu_tj') && document.getElementById('baidu_tj').remove()
+//       let hm = document.createElement('script')
+//       hm.src = 'https://hm.baidu.com/hm.js?9129e2c0bb32fff3e4170347beab141e'
+//       hm.id = 'baidu_tj'
+//       let s = document.getElementsByTagName('script')[0]
+//       s.parentNode.insertBefore(hm, s)
+//     })()
+//   }, 50)
+// })
+router.afterEach((to, from, next) => {
+  setTimeout(() => {
+    var _hmt = _hmt || [];
+    (function () {
+      document.getElementById('baidu_tj') && document.getElementById('baidu_tj').remove()
+      var hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?9129e2c0bb32fff3e4170347beab141e'
+      var s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(hm, s)
+    })()
+  }, 0)
+})
